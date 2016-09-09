d90965e619a7cba456ca3fc0f8bbbc4425fdfb93
feaco <pheaco@gmail.com>
2016-09-09 20:30:48 +0800

    add

--- public/src/scripts/controller.js
+++ public/src/scripts/controller.js
@@ -13,8 +13,11 @@ angular.module('starter.controllers', ['ionic'])
         $scope.count = 0;               //注数
         $scope.unitPrice = 2;           //单价
         $scope.multiple = 1;            //倍数
-        
+        $scope.hasBall = false;
 
+        $scope.extraisShow = true;      //倍数选择栏，ng-hide
+
+        $scope.globalCount = 0;
  
         //号码篮
         $scope.basket = {};
@@ -45,9 +48,131 @@ angular.module('starter.controllers', ['ionic'])
        }
        //机选
        $scope.getRandom = function(evt){
-        console.log('机选');
+        util.clearBalls($scope.ballTree);
+        typeSplits =  $scope.currentMethod.split('.');
+         var arr;
+         if(/fushi$/.test(typeSplits[typeSplits.length-1])){
+              //选二 - 前二直选  两组
+              if(typeSplits[0] == 'xuaner' && typeSplits[1] == 'qianerzhixuan'){
+                arr = [[],[]];
+                var rand1 = util.getRandom();;
+                arr[0].push(rand1);
+                var rand2 = util.getRandom();
+                while(rand1 == rand2){
+                  rand2 = util.getRandom();
+                }
+                arr[1].push(rand2);
+                //选一定位胆任选一个
+              }else if(typeSplits[1] == 'dingweidan'){
+                arr = [[],[],[]];
+                var rand = util.getRandom();
+                arr[Math.floor(Math.random()*4)].push(rand);
+                 //选三 - 前三直选 三组
+              }else if(typeSplits[0] == 'xuansan' && typeSplits[1] == 'qiansanzhixuan'){
+                  arr = [[],[],[]];
+                  var rand1 = util.getRandom();;
+                  arr[0].push(rand1);
+                  var rand2 = util.getRandom();
+                  while(rand1 == rand2){
+                    rand2 = util.getRandom();
+                  }
+                  arr[1].push(rand2);
+                  var rand3 = util.getRandom();
+                  while(rand3 == rand1 || rand3 == rand2){
+                    rand3 = util.getRandom();
+                  }
+                  arr[2].push(rand3);
+              }else{            //分配相应个数的随机数
+                 arr = [];
+                 arr = util.getRandArr($scope.ballTree.choosenum);
+              }
+          }else if(/danshi$/.test(typeSplits[typeSplits.length-1])){  //应该不会用到 
+            console.log('单式');
+          }else if(/dantuo$/.test(typeSplits[typeSplits.length-1])){  //两组
+            arr = [[],[]];
+            var temp = util.getRandArr($scope.ballTree.choosenum);
+            arr[0] = temp.splice(0,1);
+            arr[1] = temp;
+          }else if(typeSplits[typeSplits.length-1] == 'dingdanshuang'){          
+              arr = [];
+              arr.push(Math.floor(Math.random()*6));
+              switch(arr[0]){
+                case 0 : arr[0] = '5单0双';break;
+                case 1 : arr[0] = '4单1双';break;
+                case 2 : arr[0] = '3单2双';break;
+                case 3 : arr[0] = '2单3双';break;
+                case 4 : arr[0] = '1单4双';break;
+                case 5 : arr[0] = '0单5双';break;
+              }
+          }else if(typeSplits[typeSplits.length-1] == 'caizhongwei'){         
+              arr = [];
+              arr.push(Math.floor(Math.random()*7)+3);
+          }
+          //渲染球
+          for(var i = 0 ; i < $scope.ballTree.length; i++){
+            for(var j = 0; j<$scope.ballTree[i].balls.length; j++){
+              var len = $scope.ballTree[0].h3 == '胆码' ? arr[1].length : arr.length;  //定位胆看第二个数组的长度
+              for(var k = 0; k < len; k++){
+               if($scope.ballTree[i].balls[j].cnt == (typeof arr[i] == 'object' ? arr[i][k] : arr[k])){
+                  $scope.ballTree[i].balls[j].active = true;
+               }
+              }
+            }
+          }
+
+          $scope.basket[$scope.currentMethod] = $scope.ballTree;
+          $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
+          $scope.ballTree.choosenum = util.getChooseNumber($scope.currentMethod);
+          $scope.hasBall = util.hasBall($scope.ballTree);
       }
 
+//设置注单信息
+      $scope.setBukets = function(isdanshi){
+        if(isdanshi){
+          return;
+        }
+       
+        if($scope.count > 0 ){      //说明有合法注单
+          if(!$scope.ballTree.betsArr){
+            $scope.ballTree.betsArr = [];        
+          }else{
+             if(util.isExsits($scope.ballTree.bets,$scope.ballTree.betsArr)){
+              return;
+             }
+          } 
+
+          $scope.ballTree.bets.multiple = $scope.multiple;
+          $scope.ballTree.bets.count = $scope.count;
+          $scope.ballTree.betsArr.push($scope.ballTree.bets);
+
+          util.clearBalls($scope.ballTree);
+          $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
+          $scope.hasBall = util.hasBall($scope.ballTree);
+          $scope.globalCount = util.getGlobalCount($scope.basket);
+
+          //console.log($scope.ballTree.bets,$scope.ballTree.betsArr);
+
+        }else{          //非法注单
+          if($scope.hasBall){         //选中了部分球
+            console.log('--');
+          }else{                    //没有选择任何球
+            $scope.getRandom();
+          }
+        }
+      }
+//修改倍数
+    $scope.clickMutit = function($event){
+       $scope.extraisShow = false;
+    }
+
+//修改倍数
+    $scope.customMutiple = function(value,$event){
+      $scope.multiple += value;
+      if($scope.multiple <= 0){
+        $scope.multiple =1;
+      }
+      $scope.extraisShow = true;
+    }
        //游戏类型切换
        $scope.slideChange = function(i,gameMethods){ 
         $scope.choosedType = i;
@@ -58,6 +183,8 @@ angular.module('starter.controllers', ['ionic'])
        $scope.chooseGame = function(j,_gameType,evt){
         $scope.singleMode = false;
         $scope.ballTree  = [];
+        danma = [];
+        //$scope.hasBall = false;
 
        //大小单双全清的样式切换
         angular.element('.typeSpans').removeClass('active');
@@ -72,7 +199,8 @@ angular.module('starter.controllers', ['ionic'])
         //如果以前选择过就从以前的篮子里拿
         if($scope.basket[$scope.currentMethod]){
           $scope.ballTree = $scope.basket[$scope.currentMethod];
-          //console.log($scope.ballTree);
+          $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
+          $scope.hasBall = util.hasBall($scope.basket[$scope.currentMethod]);
           return;
         }
 
@@ -102,12 +230,14 @@ angular.module('starter.controllers', ['ionic'])
               $scope.ballTree[0].quick = [];
             //该模式下一组球
           }else if(_gameType.name == 'dingdanshuang'){          //定单双
-              $scope.ballTree = util.buildUI([''],'dingdanshuang');
+              $scope.ballTree = util.buildUI(['定单双'],'dingdanshuang');
             //该模式下一组球
           }else if(_gameType.name == 'caizhongwei'){            //猜中位
               $scope.ballTree = util.buildUI(['猜中位'],'caizhongwei');
           }
           $scope.ballTree.choosenum = util.getChooseNumber($scope.currentMethod);
+          $scope.hasBall = util.hasBall($scope.ballTree);
+          $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
         }
 
         //滑动
@@ -116,32 +246,43 @@ angular.module('starter.controllers', ['ionic'])
        }
 
        //点球
+       var danma = [];
        $scope.chooseBall = function(ballTree,ball,bt){
+        ball.active = !ball.active;
+        //胆拖
         if(bt.h3 == '胆码'){
-           for(var i = 0; i < ballTree[0].balls.length; i++){
-            if(ballTree[1].balls[i].active == true && ballTree[1].balls[i].cnt == ball.cnt){
-              ballTree[1].balls[i].active = false;
+          for(var  i = 0; i<danma.length; i++){    //是不是已经被选中的球,如果是就从数据中删除
+            if(danma[i] == ball){
+              danma.splice(i,1);
+              return;
             }
-            if(ballTree[0].balls[i] == ball){
-              continue;
-            }
-            ballTree[0].balls[i].active = false;    
           }
-         
+           if(danma.length == +ballTree.choosenum-1){     //胆码的个数
+            danma[danma.length-1].active = false;
+            danma.pop();
+           }
+           danma.push(ball);
+           for(var i = 0; i < ballTree[0].balls.length; i++){     //胆码和脱码相同
+              if(ballTree[1].balls[i].active == true && ballTree[1].balls[i].cnt == ball.cnt){
+                ballTree[1].balls[i].active = false;
+              }
+           }
         }else if(bt.h3 == '拖码'){
           for(var i = 0; i < ballTree[1].balls.length; i++){
-            if(ballTree[0].balls[i].active == true && ballTree[0].balls[i].cnt == ball.cnt){
+            if(ballTree[0].balls[i].active == true && ballTree[0].balls[i].cnt == ball.cnt){ //胆码和脱码相同
                 ballTree[0].balls[i].active = false;
+                for(var j = 0; j < danma.length; j++){
+                  if(danma[j] == ballTree[0].balls[i]){
+                    danma.splice(j,1);
+                  }
+                }
             }
           }
         }
-        ball.active = !ball.active;
+        
         $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
         $scope.basket[$scope.currentMethod] = $scope.ballTree;
-        
-        //console.log($scope.count);
-        //console.log($scope.basket);
-          
+        $scope.hasBall = util.hasBall($scope.basket[$scope.currentMethod]);
        }
 
 
@@ -169,25 +310,39 @@ angular.module('starter.controllers', ['ionic'])
           }else if(q.cnt == '全'){
             bt.balls[i].active = true;
           }else if(q.cnt == '大'){
-            if(i > 4){
-              bt.balls[i].active = true;
+            if(bt.h3 == '猜中位'){
+              if(i > 2){
+                bt.balls[i].active = true;
+              }
+            }else{
+              if(i > 4){
+                bt.balls[i].active = true;
+              }
             }
           }else if(q.cnt == '小'){
+            if(bt.h3 == '猜中位'){
+              if(i < 3){
+                bt.balls[i].active = true;
+              }
+            }else{
               if(i < 5){
                 bt.balls[i].active = true;
               }
+            }
           }else if(q.cnt == '清'){
             bt.balls[i].active = false;
           }
         }
         q.active = true;
+        $scope.count = util.setBets($scope.currentMethod,$scope.ballTree) || 0;
         $scope.basket[$scope.currentMethod] = $scope.ballTree;
+        $scope.hasBall = util.hasBall($scope.basket[$scope.currentMethod]);
       }
 
      }]).factory('util',function(){  
       return{
         buildUI:function(titleArr,type){
-           var eles = ['01','02','03','04','05','06','07','08','09','10','11','5单0双','4单1双','3单2双','1单3双','1单3双','0单5双','大','小','全','单','双','清'];
+           var eles = ['01','02','03','04','05','06','07','08','09','10','11','5单0双','4单1双','3单2双','2单3双','1单4双','0单5双','大','小','全','单','双','清'];
            var result = [];
 
            for(var i = 0 ;i<titleArr.length;i++){
@@ -247,43 +402,35 @@ angular.module('starter.controllers', ['ionic'])
 
           //复式
           if(/fushi$/.test(typeSplits[typeSplits.length-1])){
-              
-              //选一
-              if(typeSplits[0] == 'xuanyi'){
-                return this.getCounts(tree,1);
-                //选二
-              }else if(typeSplits[0] == 'xuaner'){
+              //选二 - 前二直选
+              if(typeSplits[0] == 'xuaner' && typeSplits[1] == 'qianerzhixuan'){
                 var counts = 0;
-                if(typeSplits[1] == 'qianerzhixuan'){
-                   for( var i = 0; i < tree.bets.length; i++){
-                    if(tree.bets[i].length > 0){
-                      if(tree.bets[i+1] && tree.bets[i+1].length > 0){
-                        if(tree.bets[i].length == 1 && tree.bets[i+1].length == 1){
-                          if(tree.bets[i][0] != tree.bets[i+1][0]){
-                            return counts = 1;
-                          }
-                        }else{
-                          for(var i = 0; i < tree.bets[0].length; i++){
-                            for(var j = 0; j < tree.bets[1].length; j++){
-                              var temp = [];
-                              temp.push(tree.bets[0][i]);
-                              temp.push(tree.bets[1][j]);
-                              if(temp[0] != temp[1]){
-                                counts++;
-                              }
+                 for( var i = 0; i < tree.bets.length; i++){
+                  if(tree.bets[i].length > 0){
+                    if(tree.bets[i+1] && tree.bets[i+1].length > 0){
+                      if(tree.bets[i].length == 1 && tree.bets[i+1].length == 1){
+                        if(tree.bets[i][0] != tree.bets[i+1][0]){
+                          return counts = 1;
+                        }
+                      }else{
+                        for(var i = 0; i < tree.bets[0].length; i++){
+                          for(var j = 0; j < tree.bets[1].length; j++){
+                            var temp = [];
+                            temp.push(tree.bets[0][i]);
+                            temp.push(tree.bets[1][j]);
+                            if(temp[0] != temp[1]){
+                              counts++;
                             }
                           }
-                          return counts;
                         }
+                        return counts;
                       }
                     }
                   }
-                  return counts;
-                }else{
-                  return this.getCounts(tree,2);
                 }
-              }else if(typeSplits[0] == 'xuansan'){
-                if(typeSplits[1] == 'qiansanzhixuan'){
+                return counts;
+                //选三 - 前三直选
+              }else if(typeSplits[0] == 'xuansan' && typeSplits[1] == 'qiansanzhixuan'){
                   var counts = 0;
                   var arr = [];
                   for(var i = 0; i < tree.bets[0].length; i++){
@@ -301,46 +448,31 @@ angular.module('starter.controllers', ['ionic'])
                       }
                     }
                   }
-                  //console.log(arr);
                   return counts;
-                }else{
-                  return this.getCounts(tree,3);
-                }
-              }else if(typeSplits[0] == 'xuansi'){
-                return this.getCounts(tree,4);
-              }else if(typeSplits[0] == 'xuanwu'){
-                return this.getCounts(tree,5);
-              }else if(typeSplits[0] == 'xuanliu'){
-                return this.getCounts(tree,6);
-              }else if(typeSplits[0] == 'xuanqi'){
-                return this.getCounts(tree,7);
-              }else if(typeSplits[0] == 'xuanba'){
-                return this.getCounts(tree,8);
-              }else if(typeSplits[0] == 'quwei'){
-                console.log('趣味');
+              }else{
+                 return this.getCounts(tree,tree.choosenum);
               }
 
           }else if(/danshi$/.test(typeSplits[typeSplits.length-1])){  //应该不会用到 
             console.log('单式');
           }else if(/dantuo$/.test(typeSplits[typeSplits.length-1])){
-            var arr = [],flag = false;
+            var arr = [],arr1 = [];
             for(var i = 0; i < tree[0].balls.length; i++){
-              if(tree[1].balls[i].active == true){
-                arr.push(tree[1].balls[i].cnt);
-              }
               if(tree[0].balls[i].active == true){
-                flag = true;
+               arr.push(tree[0].balls[i].cnt);
               }
+              if(tree[1].balls[i].active == true){
+                arr1.push(tree[1].balls[i].cnt);
+              }              
             }
-            if(flag && arr.length >= tree.choosenum){
-              //console.log(tree);
-              console.log(this.getCounts(tree,tree.choosenum));
+            if(arr.length + arr1.length >= tree.choosenum){
+             return this.getCombine(arr1.length,tree.choosenum-arr.length);
             }
 
           }else if(typeSplits[typeSplits.length-1] == 'dingdanshuang'){          
-              console.log('定单双');
+              return this.getCounts(tree,tree.choosenum);
           }else if(typeSplits[typeSplits.length-1] == 'caizhongwei'){         
-              console.log('猜中位');
+              return this.getCounts(tree,tree.choosenum);
           }
         },
         getBallLen:function(tree){     //选了哪些球 [[],[],[]];
@@ -378,8 +510,66 @@ angular.module('starter.controllers', ['ionic'])
             }
           }
           return counts;
+        },
+        hasBall:function(tree){
+          for(var i = 0; i < tree.length; i++){
+            for(var j = 0; j < tree[i].balls.length; j++){
+              if(tree[i].balls[j].active == true){
+                return true;
+              }
+            }
+          }
+          return false;
+        },
+        getRandom:function(){
+          return Math.floor(Math.random()*11)+1;
+        },
+        getRandArr:function(tree){
+          var arr = [];
+          for(var i = 0; i < tree; i++){
+            var rand = this.getRandom();
+            for( var j = 0; j < arr.length; j++){
+              if(rand == arr[j]){
+                rand = this.getRandom();
+                j = -1;
+              }
+            }
+            arr.push(rand);
+          }
+          return arr;
+        },
+        clearBalls:function(tree){          //清除球
+          for(var i = 0 ; i < tree.length; i++){
+            for(var j = 0; j<tree[i].balls.length; j++){
+              tree[i].balls[j].active = false;
+            }
+          }
+        },
+        isExsits:function(tree,trees){    //注单是否存在
+          for(var i = 0; i < trees.length; i++){
+            if(tree.toString() == trees[i].toString()) {   //[[],[],[]]
+              var flag = true;
+              for(var j = 0; j < trees[i].length; j++) {
+                if(tree[j].toString() != trees[i][j].toString()){
+                  flag = false;
+                  break;
+                }
+              }
+             if(!flag){
+              continue;
+             }
+              return true;
+            }      
+          }
+          return false;
+        },
+        getGlobalCount:function(trees){   //获取中的注数
+            var counts = 0;
+           for(var p in trees){
+              counts += trees[p]['betsArr'].length;
+           }
+           return counts;
         }
-
       }
     })
      .controller('drawCtrl', ['$scope', function ($scope) {
